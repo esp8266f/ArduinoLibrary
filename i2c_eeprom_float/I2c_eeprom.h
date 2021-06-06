7ac28537bbf30eee8e9b81010170ad9830986b8c
esp8266f <75428014+esp8266f@users.noreply.github.com>
2021-06-06 10:57:37 +0300

    Add files via upload

--- i2c_eeprom_float/I2c_eeprom.h
+++ i2c_eeprom_float/I2c_eeprom.h
@@ -8,68 +8,68 @@
 	FOR PERSONAL USE
 */
 
-	/*
-	description:
-	
-	Arduino class for write data to external eeprom in i2c bus (zs-042 module)(ds3231).
-	
-	// FLOAT -> 4 byte
-	// zs-042 => first 16 byte use DS3231
-	
-	// example for use:
-	
-	#include "I2C_eeprom.h"
-	#define DEVICE1_ADDRESS 0x57
-	I2C_eeprom i2c_eeprom;		// initialization
-	
-	void setup() {
-		Serial.begin(115200);
-		i2c_eeprom.float_put(DEVICE1_ADDRESS, 20, 3.14);
-		i2c_eeprom.float_put(DEVICE1_ADDRESS, 24, 7.64);
-		i2c_eeprom.float_put(DEVICE1_ADDRESS, 28, 22.4);
-		
-		Serial.println(i2c_eeprom.float_get(DEVICE1_ADDRESS, 20));
-		i2c_eeprom.float_update(DEVICE1_ADDRESS, 20, 780.25);
-		boolean avaible = i2c_eeprom.is_avaible(DEVICE1_ADDRESS);
-		String text = i2c_eeprom.device_scanner();
-		Serial.println(avaible);
-		Serial.println(text);
-	}
-	
-	void loop() {
-		
-	}
-	
-	
-	*/
-	
+/*
+  description:
 
-class I2C_eeprom{
-	public:
-		I2C_eeprom();
-		boolean is_avaible(int device_address);
-		void float_put(int device_address, unsigned int address, float value);
-		float float_get(int device_address, unsigned int address);
-		void float_update(int device_address, unsigned int address, float value);
-		String device_scanner();
-			
+  Arduino class for write data to external eeprom in i2c bus (zs-042 module)(ds3231).
+
+  // FLOAT -> 4 byte
+  // zs-042 => first 16 byte use DS3231
+
+  // example for use:
+
+  #include "I2C_eeprom.h"
+  #define DEVICE1_ADDRESS 0x57
+  I2C_eeprom i2c_eeprom;		// initialization
+
+  void setup() {
+	Serial.begin(115200);
+	i2c_eeprom.float_put(DEVICE1_ADDRESS, 20, 3.14);
+	i2c_eeprom.float_put(DEVICE1_ADDRESS, 24, 7.64);
+	i2c_eeprom.float_put(DEVICE1_ADDRESS, 28, 22.4);
+
+	Serial.println(i2c_eeprom.float_get(DEVICE1_ADDRESS, 20));
+	i2c_eeprom.float_update(DEVICE1_ADDRESS, 20, 780.25);
+	boolean avaible = i2c_eeprom.is_avaible(DEVICE1_ADDRESS);
+	String text = i2c_eeprom.device_scanner();
+	Serial.println(avaible);
+	Serial.println(text);
+  }
+
+  void loop() {
+
+  }
+
+
+*/
+
+
+class I2C_eeprom {
+  public:
+    I2C_eeprom();
+    boolean is_avaible(int device_address);
+    void float_put(int device_address, unsigned int address, float value);
+    float float_get(int device_address, unsigned int address);
+    void float_update(int device_address, unsigned int address, float value);
+    String device_scanner();
+
 };
 
-	// init
-	I2C_eeprom::I2C_eeprom(){				// constructor
-		 Wire.begin();
-	}
-	
-	// device_address is avaible in i2c line
-	boolean I2C_eeprom::is_avaible(int device_address) {
-		boolean is_avaible = false;
-		Wire.beginTransmission(device_address);
-		byte error = Wire.endTransmission();
-		if (error == 0) is_avaible = true;
-		return is_avaible;
-	}
-	
-	// WRITE FLOAT!!!!*******************************
+// init
+I2C_eeprom::I2C_eeprom() {				// constructor
+  Wire.begin();
+}
+
+// device_address is avaible in i2c line
+boolean I2C_eeprom::is_avaible(int device_address) {
+  boolean is_avaible = false;
+  Wire.beginTransmission(device_address);
+  byte error = Wire.endTransmission();
+  if (error == 0) is_avaible = true;
+  return is_avaible;
+}
+
+// WRITE FLOAT!!!!*******************************
 void I2C_eeprom::float_put(int device_address, unsigned int address, float value) {
   byte *byte_data = (byte *)&value; // (value) convert float to byte
   for (int i = 0; i < 4; i++) {
--- i2c_eeprom_float/i2c_eeprom_float.ino
+++ i2c_eeprom_float/i2c_eeprom_float.ino
@@ -8,9 +8,9 @@ I2C_eeprom i2c_eeprom;    // initialization
 
 void setup() {
   Serial.begin(115200);
-//  i2c_eeprom.float_put(DEVICE1_ADDRESS, 20, 3.14);
-//  i2c_eeprom.float_put(DEVICE1_ADDRESS, 24, 7.64);
-//  i2c_eeprom.float_put(DEVICE1_ADDRESS, 28, 22.4);
+  //  i2c_eeprom.float_put(DEVICE1_ADDRESS, 20, 3.14);
+  //  i2c_eeprom.float_put(DEVICE1_ADDRESS, 24, 7.64);
+  //  i2c_eeprom.float_put(DEVICE1_ADDRESS, 28, 22.4);
 
   Serial.println("[20] => " + String(i2c_eeprom.float_get(DEVICE1_ADDRESS, 20)));
   Serial.println("[24] => " + String(i2c_eeprom.float_get(DEVICE1_ADDRESS, 24)));
@@ -19,10 +19,10 @@ void setup() {
   Serial.println("update [20]");
   i2c_eeprom.float_update(DEVICE1_ADDRESS, 20, 780.25);
   Serial.println("[20] => " + String(i2c_eeprom.float_get(DEVICE1_ADDRESS, 20)));
-  
+
   boolean avaible = i2c_eeprom.is_avaible(DEVICE1_ADDRESS);
   Serial.println("[0x57] is avaible: " + String(avaible));
-  
+
   String scanner_text = i2c_eeprom.device_scanner();
   Serial.println(scanner_text);
 }
